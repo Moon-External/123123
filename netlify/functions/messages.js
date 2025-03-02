31a2d2015e731e98aeac8fcfd84a16be8b18b330
Moon <boskagame@yandex.ru>
2025-03-02 21:06:33 +0300

    Update messages.js

--- netlify/functions/messages.js
+++ netlify/functions/messages.js
@@ -1,1 +1,40 @@
+const fs = require('fs');
+const path = require('path');
 
+const messagesFilePath = path.join(process.cwd(), 'messages.json');
+
+function loadMessages() {
+    if (!fs.existsSync(messagesFilePath)) {
+        fs.writeFileSync(messagesFilePath, '[]');
+    }
+    const data = fs.readFileSync(messagesFilePath);
+    return JSON.parse(data);
+}
+
+function saveMessages(messages) {
+    fs.writeFileSync(messagesFilePath, JSON.stringify(messages, null, 2));
+}
+
+exports.handler = async (event) => {
+    if (event.httpMethod === 'GET') {
+        const messages = loadMessages();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(messages),
+        };
+    } else if (event.httpMethod === 'POST') {
+        const { username, message } = JSON.parse(event.body);
+        const messages = loadMessages();
+        messages.push({ username, message });
+        saveMessages(messages);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ success: true }),
+        };
+    } else {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed',
+        };
+    }
+};
